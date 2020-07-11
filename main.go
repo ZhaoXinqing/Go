4eac1fd53911a278e7318acfd9e5bdab8ce8328b
ZhaoXinqing <1375565592@qq.com>
2020-07-11 11:05:31 +0800

    多线程ABCD

--- main.go
+++ main.go
@@ -1,26 +1,84 @@
-data := os.Environ()
-for _, val := range data {
-	fmt.Println(val)
-}
-fmt.Println("___________________end_____________environ_________")
+package main
 
-mapping := func(s string) string {
-	m := map[string]string{"xx":"ssssssssssssss",
-		"YY":"TTTTTTTTTTTTTT"}
-	return m[s]
-}
-datas := "hello $xx blog address $yy"
-expandStr := os.Expand(datas, mapping)
-fmt.Println(expandStr)
+import (
+	"fmt"
+	"runtime"
+)
 
-err = os.Link("main.go","newmain.go")
-if err != nil {
-	fmt.Println(err)
+var ch chan string = make(chan string)
+
+var ch1 chan int = make(chan int)
+var ch2 chan int = make(chan int)
+var ch3 chan int = make(chan int)
+var ch4 chan int = make(chan int)
+
+func sendA() {
+	for {
+		c := <-ch1
+		if c == 1 {
+			ch <- "A"
+			ch2 <- 1
+		}
+		runtime.Gosched()
+	}
 }
 
-var pathSep string
-if os.IsPathSeparator("\\") {
-	pathSeq = "\\"
-} else {
-	pathSeq = "/"
+func sendB() {
+	for {
+		c := <-ch2
+		if c == 1 {
+			ch <- "B"
+			ch3 <- 1
+		}
+		runtime.Gosched()
+	}
+
+}
+
+func sendC() {
+	for {
+		c := <-ch3
+		if c == 1 {
+			ch <- "C"
+			ch4 <- 1
+		}
+		runtime.Gosched()
+	}
+}
+
+func sendD() {
+	for {
+		c := <-ch4
+		if c == 1 {
+			ch <- "D"
+			ch1 <- 1
+		}
+		runtime.Gosched()
+	}
+}
+
+func main() {
+	go sendA()
+	go sendB()
+	go sendC()
+	go sendD()
+	ch1 <- 1
+
+	var n int
+	for {
+		_, err := fmt.Scan(&n)
+		if err != nil {
+			break
+		}
+
+		if n == 0 {
+			continue
+		}
+
+		for i := 0; i < n*4; i++ {
+			str := <-ch
+			fmt.Printf("%s", str)
+		}
+		fmt.Println()
+	}
 }
