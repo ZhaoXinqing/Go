26c10b5abbb5e59d5f9d16b261913945e53980b2
ZhaoXinqing <1375565592@qq.com>
2020-09-13 11:39:33 +0800

    bird

--- 1-Go基础/4-demo/3-遍历/遍历文件夹/fidir.go
+++ 1-Go基础/4-demo/3-遍历/遍历文件夹/fidir.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -29,4 +31,25 @@ func main() {
 	dir := `F:\1-财经知识`
 	findDir(dir, 0) // 0为要遍历的层级，默认是0
 	fmt.Println(time.Now())
+}
+
+// 并发遍历
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "du1:%v\n", err)
+		return nil
+	}
+	return entries
 }
