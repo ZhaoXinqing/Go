cc04915165cf855389854c7ecaffe5ae70e07eb4
zhaoxinqing <1375565592@qq.com>
2020-10-26 17:12:10 +0800

    参数获取方式

---
+++ changjing.ai/service/tag/util.go
@@ -0,0 +1,12 @@
+import "context"
+
+// DeleteBoardPermission ...
+func (h *Handler) DeleteBoardPermission(ctx *context.Context) {
+	var (
+		boardID         = ctx.UserValue[guard.BoardIDKey].(int64) // 从 url 中获取参数
+		userIDParam     = guard.GetUserIDParam(ctx)               // 从 body 中获取参数
+		boardPermission = models.BoardPermissionsModelWithDriver(h.conn)
+	)
+	boardPermission.DeleteDB(`"BoardID" = ? and "UserID" = ?`, boardID, userIDParam.UserID)
+	response.OkWithData(ctx, map[string]interface{}{})
+}
