6e23e86f21b3d9891f78555d5657dc25f82130bf
“zhaoxinqing” <1375565592@qq.com>
2020-11-10 19:19:38 +0800

    aajsoijdfiou

--- 1-Go基础/map/Untitled-1.go
+++ 1-Go基础/map/Untitled-1.go
@@ -1,23 +1,3 @@
-package map
-
-import "fmt"
-
-//RemoveRepByMap slice去重
-func RemoveRepByMap(slc []int64) []int64 {
-	result := []int64{}         //存放返回的不重复切片
-	tempMap := map[int64]byte{} // 存放不重复主键
-	for _, e := range slc {
-		l := len(tempMap)
-		tempMap[e] = 0 //当e存在于tempMap中时，再次添加是添加不进去的，，因为key不允许重复
-		//如果上一行添加成功，那么长度发生变化且此时元素一定不重复
-		if len(tempMap) != l { // 加入map后，map长度变化，则元素不重复
-			result = append(result, e) //当元素不重复时，将元素添加到切片result中
-		}
-	}
-	return result
-}
-
-
 //AddressResolution 地址解析
 func AddressResolution(address, city string) (lng, lat float64) {
 	params := map[string]string{}
@@ -44,6 +24,10 @@ func AddressResolution(address, city string) (lng, lat float64) {
 	return lng, lat
 }
 
+
+
+
+
 // [1]
 if res, err := Get(URL, params, nil); err == nil {
 	defer res.Body.Close()
@@ -60,15 +44,29 @@ if err = odysseyUtils.Unmarshal(body, &gaodeGeoRes); err == nil {
 }
 // [2]
 if res, err := Get(URL, params, nil); err == nil {
-	defer res.Body.Close()
 	if body, err := ioutil.ReadAll(res.Body); err == nil {
 		gaodeGeoRes := domain.GaodeGeoRes{}
 	}
 }
 err = odysseyUtils.Unmarshal(body, &gaodeGeoRes)
-if err == nil && gaodeGeoRes.Status == "1" len(gaodeGeoRes.Geocodes) != 0 {
-		latlngStrs := strings.Split(gaodeGeoRes.Geocodes[0].Location, ",")
-		lng, _ = strconv.ParseFloat(latlngStrs[0], 64)
-		lat, _ = strconv.ParseFloat(latlngStrs[1], 64)
-	}
+if err == nil && gaodeGeoRes.Status == "1"  &&  len(gaodeGeoRes.Geocodes) != 0 {
+	latlngStrs := strings.Split(gaodeGeoRes.Geocodes[0].Location, ",")
+	lng, _ = strconv.ParseFloat(latlngStrs[0], 64)
+	lat, _ = strconv.ParseFloat(latlngStrs[1], 64)
 }
+defer res.Body.Close()
+return lng, lat
+
+// 【3】
+res, err := Get(URL, params, nil)
+if err == nil && body, err2 := ioutil.ReadAll(res.Body); err2 == nil {
+	gaodeGeoRes := domain.GaodeGeoRes{}
+}
+err = odysseyUtils.Unmarshal(body, &gaodeGeoRes)
+if err == nil && gaodeGeoRes.Status == "1"  &&  len(gaodeGeoRes.Geocodes) != 0 {
+	latlngStrs := strings.Split(gaodeGeoRes.Geocodes[0].Location, ",")
+	lng, _ = strconv.ParseFloat(latlngStrs[0], 64)
+	lat, _ = strconv.ParseFloat(latlngStrs[1], 64)
+}
+defer res.Body.Close()
+return lng, lat
