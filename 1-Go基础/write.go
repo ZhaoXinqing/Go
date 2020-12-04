6811c58cd921bbc4139f80f708898041ab2ce5dc
“zhaoxinqing” <1375565592@qq.com>
2020-12-04 13:57:28 +0800

    OpenFile

--- 1-Go基础/write.go
+++ 1-Go基础/write.go
@@ -147,3 +147,24 @@ func main() {
 		fmt.Println("File writing failed")
 	}
 }
+
+// Golang的OpenFile函数写入默认是追加的
+
+// os.O_TRUNC 覆盖写入，不加则追加写入
+
+func WriteToFile(fileName string, content string) error {
+	f, err := os.OpenFile(fileName, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("file create failed. err: " + err.Error())
+	} else {
+		// offset
+		//os.Truncate(filename, 0) //clear
+		n, _ := f.Seek(0, os.SEEK_END)
+		_, err = f.WriteAt([]byte(content), n)
+		fmt.Println("write succeed!")
+		defer f.Close()
+	}
+	return err
+}
+
+// https://www.jianshu.com/p/ad2e2ad7dd07
